78e313ded9ea420a72aa447bc5743a504dbae32b
nlawler1737 <72905287+nlawler1737@users.noreply.github.com>
2024-08-30 17:31:54 -0700

    clean up, refactor

--- BP/scripts/elevators/elevator.ts
+++ BP/scripts/elevators/elevator.ts
@@ -43,67 +43,80 @@ function detectOnElevator(): void {
     if (!config.blocks.size) return
     world.getAllPlayers().forEach(player => {
         const heightRange = player.dimension.heightRange
+        const isJumpingXorSneaking = xor(player.isJumping, player.isSneaking)
 
         // round y pos to prevent bugs on some y locations
         const playerLocation = player.location
         playerLocation.y = round(playerLocation.y, 0.00001)
         const playerY = playerLocation.y
 
+
+        // set teleport ready to true if player is not sneaking or jumping
         if (!player.isJumping && !player.isSneaking) {
             player.setDynamicProperty(PROPERTY_TELEPORT_READY, true)
-        } else if (((player.isJumping && !player.isSneaking) || (player.isSneaking && !player.isJumping)) && player.getDynamicProperty(PROPERTY_TELEPORT_READY) && playerY <= heightRange.max && playerY > heightRange.min) {
-            const blockBelow = player.dimension.getBlock(Vector.add(Vector.down, playerLocation))
-            const blockBelowType = blockBelow.typeId
-            if (player.isSneaking && blockBelow.location.y === -64) return
+            return
+        }
 
-            if (!config.blocks.has(blockBelowType)) {
-                player.setDynamicProperty(PROPERTY_TELEPORT_READY, false)
+        const isPlayerBetweenMinAndMaxY = playerY <= heightRange.max && playerY > heightRange.min
+        const isTeleportReady = player.getDynamicProperty(PROPERTY_TELEPORT_READY)
+
+        if (!isJumpingXorSneaking || !isTeleportReady || !isPlayerBetweenMinAndMaxY) {
+            return
+        }
+
+        const blockBelow = player.dimension.getBlock(Vector.add(Vector.down, playerLocation))
+        const blockBelowType = blockBelow.typeId
+
+        if (player.isSneaking && blockBelow.location.y === -64) return
+
+        if (!config.blocks.has(blockBelowType)) {
+            player.setDynamicProperty(PROPERTY_TELEPORT_READY, false)
+            return
+        }
+
+        let block = getNearestBlockType(blockBelow, player.isJumping ? Direction.Up : player.isSneaking ? Direction.Down : null)
+
+        player.setDynamicProperty(PROPERTY_TELEPORT_READY, false)
+
+        if (!block || block.typeId !== blockBelowType) return
+
+        try {
+            if (!block.above().isAir || !block.above().above().isAir) {
+                player.sendMessage("§cElevator Obstructed")
                 return
             }
+        } catch (e) { }
 
-            let block = getNearestBlockType(blockBelow, player.isJumping ? Direction.Up : player.isSneaking ? Direction.Down : null)
+        const tpLocation = Vector.add(playerLocation, Vector.subtract(block.location, blockBelow.location))
+        const directionState = block.permutation.getState(BLOCK_STATE_TELEPORT_DIRECTION) as number
 
-            player.setDynamicProperty(PROPERTY_TELEPORT_READY, false)
-
-            if (!block || block.typeId !== blockBelowType) return
-
-            try {
-                if (!block.above().isAir || !block.above().above().isAir) {
-                    player.sendMessage("§cElevator Obstructed")
-                    return
-                }
-            } catch (e) { }
-
-            const tpLocation = Vector.add(playerLocation, Vector.subtract(block.location, blockBelow.location))
-
-            const directionState = block.permutation.getState(BLOCK_STATE_TELEPORT_DIRECTION) as number
-            if (config.teleportAllOnBlock) {
-                player.dimension.getEntitiesAtBlockLocation(playerLocation).forEach(entity => {
-                    if (entity instanceof Player) {
-                        if (!config.teleportPlayers && entity !== player) return
-                        if (directionState) {
-                            entity.teleport(tpLocation, { rotation: ROTATIONS[directionState] })
-                        } else {
-                            entity.teleport(tpLocation)
-                        }
+        if (config.teleportAllOnBlock) {
+            player.dimension.getEntitiesAtBlockLocation(playerLocation).forEach(entity => {
+                if (entity instanceof Player) {
+                    if (!config.teleportPlayers && entity !== player) return
+                    if (directionState) {
+                        entity.teleport(tpLocation, { rotation: ROTATIONS[directionState] })
                     } else {
-                        entity.teleport(Vector.add(block.bottomCenter(), Vector.up))
+                        entity.teleport(tpLocation)
                     }
-                })
-            } else {
-                if (directionState) {
-                    player.teleport(tpLocation, { rotation: ROTATIONS[directionState] })
-
                 } else {
-                    player.teleport(tpLocation)
-
+                    entity.teleport(Vector.add(block.bottomCenter(), Vector.up))
                 }
+            })
+        } else {
+            if (directionState) {
+                player.teleport(tpLocation, { rotation: ROTATIONS[directionState] })
+
+            } else {
+                player.teleport(tpLocation)
+
             }
-            system.runTimeout(() => {
-                player.dimension.playSound("mob.endermen.portal", block.center(), { volume: 0.5, pitch: 2.5 })
-                player.dimension.playSound("mob.endermen.portal", blockBelow.center(), { volume: 0.5, pitch: 2.5 })
-            }, 1)
         }
+
+        system.runTimeout(() => {
+            player.dimension.playSound("mob.endermen.portal", block.center(), { volume: 0.5, pitch: 2.5 })
+            player.dimension.playSound("mob.endermen.portal", blockBelow.center(), { volume: 0.5, pitch: 2.5 })
+        }, 1)
     })
 }
 
@@ -174,4 +187,8 @@ function round(num: number, tolerance: number) {
     const diff = Math.abs(rounded - num)
     if (diff > tolerance) return num
     return rounded
+}
+
+function xor(a: boolean, b: boolean) {
+    return a ? !b : b
 }
